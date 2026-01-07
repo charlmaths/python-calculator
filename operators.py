851963b546539295d4eb10cad7af6f46605eadb7
charlmaths <charlzen.martinez@gmail.com>
2026-01-07 10:49:13 +0000

    Updates operator class, defined basic mathematics method

--- operators.py
+++ operators.py
@@ -1,4 +1,25 @@
 # class operators
 
 class Operators:
-    pass
+    '''Basic Operator Class'''
+    def __init__(self):
+        self.num1 = 0
+        self.num2 = 0
+
+
+    def add(self):
+        return self.num1 + self.num2
+
+
+    def subtract(self):
+        return self.num1 - self.num2
+
+
+    def multiply(self):
+        return self.num1 * self.num2
+
+
+    def divide(self):
+        return self.num1 / self.num2
+
+
