763748401fdf5a9ce17eacafedbbaabc0d1940f9
charlmaths <charlzen.martinez@gmail.com>
2026-01-10 22:40:05 +0000

    Updates calculator, created runner function to handle choices

--- calculator.py
+++ calculator.py
@@ -1,15 +1,17 @@
 from basicmathoperations import Operators
 
 
-def basic_operation(operator, num1, num2):
+def basic_operation(operator):
     '''Basic Arithmetic'''
+    # num1 = float(input("Enter first number: "))
+    # num2 = float(input("Enter second number: "))
     while operator != 5:
+
         if operator == 1:
             print("You have chosen to add")
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
-            adding = Operators(num1, num2)
-            res = adding.add()
+            res = Operators(num1, num2).add()
             print(f"Result: {res}")
             operator = int(input("Another calculation? Enter choice(1/2/3/4/5): "))
 
@@ -17,26 +19,29 @@ def basic_operation(operator, num1, num2):
             print("You have chosen to subtract")
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
-            subtracting = Operators(num1, num2)
-            res = subtracting.subtract()
+            res = Operators(num1, num2).subtract()
             print(f"Result: {res}")
             operator = int(input("Another calculation? Enter choice(1/2/3/4/5): "))
+
         elif operator == 3:
             print("You have chosen to multiply")
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
-            multiplying = Operators(num1, num2)
-            res = multiplying.multiply()
+            res = Operators(num1, num2).multiply()
             print(f"Result: {res}")
             operator = int(input("Another calculation? Enter choice(1/2/3/4/5): "))
+
         elif operator == 4:
             print("You have chosen to divide")
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
-            dividing = Operators(num1, num2)
-            res = dividing.divide()
+            res = Operators(num1, num2).divide()
             print(f"Result: {res}")
             operator = int(input("Another calculation? Enter choice(1/2/3/4/5): "))
+
+        elif operator == 5:
+            print("Exiting...")
+            break
         else:
             return "Invalid input, exiting..."
 
@@ -44,29 +49,44 @@ def basic_operation(operator, num1, num2):
 
 def runner():
     ''' initiator '''
-    operator = int(input("Enter choice(1/2/3/4/5): "))
-    return operator
+    print("Please pick a calculator:\n")
+    print("Calculator Menu:\n")
+    print('''
+    1. Basic Arithmetic Calculator
+    2. Geometric Calculator
+    3. Exit
+    \n''')
+    calc_choice = int(input("Enter choice(1/2/3): "))
+
+    if calc_choice == 1:
+        print("Select operation:\n")
+        print('''
+            1. Add
+            2. Subtract
+            3. Multiply
+            4. Divide
+            5. Exit
+            \n''')
+        operator = int(input("Enter choice(1/2/3/4/5): "))
+        return operator
+
+    elif calc_choice == 2:
+        print("Geometric Calculator is under development. Exiting...")
+        exit()
+    elif calc_choice == 3:
+        print("Exiting...")
+        exit()
 
 
 def main():
     ''' main function '''
-    print("Please pick the operation -\n")
-    print('''
-    1. Add
-    2. Subtract
-    3. Multiply
-    4. Divide
-    5. Exit
-    ''')
-    operator = runner()
-    basic_op = basic_operation(operator, 0, 0)
+    choice = runner()
+    basic_op = basic_operation(choice)
     return basic_op
-
 
 if __name__ == "__main__":
     main()
 
-# print("Exiting the calculator. Goodbye!")
 
 
 
